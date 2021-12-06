2c97348fb92f4516e01a42686e0033b7347a7e23
ramon oliveira <ramon30012000@gmail.com>
2021-12-06 18:10:28 -0300

    ...

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ function App() {
         <div>
             <Navbar/>
             <Corpo/>
+            <div class="fundo-mobile">
+                <ion-icon name="home"></ion-icon>
+                <ion-icon name="search-outline"></ion-icon>
+                <ion-icon name="add-circle-outline"></ion-icon>
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="person-outline"></ion-icon>
+            </div>
         </div>
     )
 }
